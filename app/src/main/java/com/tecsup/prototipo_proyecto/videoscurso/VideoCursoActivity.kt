47c878e26bf3eb62627471680541e51ac1550ca9
Alm4en0 <almendra.garcia@tecsup.edu.pe>
2024-06-18 00:08:31 -0500

    Pantalla de video de Curso

--- app/src/main/java/com/tecsup/prototipo_proyecto/videoscurso/VideoCursoActivity.kt
+++ app/src/main/java/com/tecsup/prototipo_proyecto/videoscurso/VideoCursoActivity.kt
@@ -7,50 +7,29 @@ import android.widget.Button
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.tecsup.prototipo_proyecto.R
+import com.tecsup.prototipo_proyecto.R.id.customMediaController
 
 class VideoCursoActivity : AppCompatActivity() {
 
     private lateinit var videoView: VideoView
-    private lateinit var btnPlay: Button
-    private lateinit var btnPause: Button
-    private lateinit var btnFullscreen: Button
     private lateinit var customMediaController: CustomMediaController
-    private var isFullScreen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_curso)
 
         videoView = findViewById(R.id.videoView)
-        btnPlay = findViewById(R.id.btnPlay)
-        btnPause = findViewById(R.id.btnPause)
-        btnFullscreen = findViewById(R.id.btnFullscreen)
         customMediaController = findViewById(R.id.customMediaController)
 
         setupVideoView()
-        setupButtons()
     }
-
     private fun setupVideoView() {
-        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.videoejemplo)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.ejemplo2)
         videoView.setVideoURI(videoUri)
 
         customMediaController.setVideoView(videoView)
         customMediaController.showControls() // Mostrar los controles inicialmente
 
-        videoView.start()
     }
 
-    private fun setupButtons() {
-        btnPlay.setOnClickListener { videoView.start() }
-        btnPause.setOnClickListener { videoView.pause() }
-        btnFullscreen.setOnClickListener {
-            isFullScreen = !isFullScreen
-            if (isFullScreen) {
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-            } else {
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-            }
-        }
-    }
 }
